0233b2558d5542c334ca854ea6d048f442095625
vedantvallal <vedantvallal11@gmail.com>
2025-06-07 13:51:00 +0530

    Updated About sec

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 import './header.css';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import img1 from './assets/img1.jpg';
+import img1 from './assets/img1.jpg';
+import skillback from './assets/back.jpeg';
 // import { runHeaderAnimations } from './gsapAnimations';
 
 
@@ -195,33 +196,59 @@ useEffect(() => {
     <div className="about">   
        <div id="main">
         <div id="top">
-            <h1 id="top-h1">ABOUTME</h1>
+            <h1 id="top-h1">ABOUT</h1>
         </div>
         <div id="center">
             <div class="content">
-                <h4>GRAVITY</h4>
-                <h3><i>Browse</i> the work that define a <i>movement</i> and created a craft.</h3>
-                <div class="btn">
-                    <h5>ENTER GALLERY</h5>
-                </div>
-                <h2>(17)</h2>
+                <div className="about">
+     
+
+
+     
+       <div className="r">
+
+      <div className="data">
+        <div className="txt">
+          <h1>About <span className='color1'>Me</span></h1>
+
+        <p>
+          <h3></h3>
+        <span className='color2'>  Hey! I’m Vedant —</span> a passionate web designer who loves crafting visually <br /> appealing and  smooth user experiences. I believe a great website <br /> isn't just about how it looks, but how it feels.
+          <br />
+          <br />
+          My approach combines creativity with clean code. Whether it’s <br /> a landing page, a portfolio, or a full-blown UI design, I focus on simplicity,<br /> structure, and speed.
+          <br />
+          <br />
+        
+          Designing intuitive, user-friendly websites is more than just a skill for me — it’s <br />something I genuinely enjoy. Every project is a new canvas, and I put my best <br />into making it pixel-perfect and responsive across all screens.
+          <br />
+          <br />
+          I’m constantly learning, improving, and staying up-to-date <br /> with the latest design trends.<br />
+
+        </p>
+
+        </div>
+          <div className="photo">
+            <img src={img1} alt="My image" />
+         </div>
+
+
+      </div>
+
+      </div>
+    </div>
             </div>
         </div>
         <div id="bottom">
-            <h1 id="bottom-h1">ABOUTME</h1>
-        </div>
-
-
-
-
-        
+            <h1 id="bottom-h1">ABOUT</h1>
+        </div>    
     </div>
-
-
-
-    
    </div>
 
+    {/* THis is Skills Section  */}
+    <div className="skills">
+       <img src={skillback} alt="My image" />
+    </div>
 
    
    </div>
